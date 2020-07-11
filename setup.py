70685046684d58caacc18afadd27a12fae6577f2
ayonge3 <ayonge3@gatech.edu>
2020-07-11 12:06:10 -0400

    setup file

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='gryphon',
+	version='0.1',
+	description='test if rk time step works correctly!',
+	url='http://github.com/ayonge3/gryphon-project',
+	author='not me',
+	author_email='not this email',
+	license='MIT',
+	packages=['gryphon'],
+	zip_safe=False)
